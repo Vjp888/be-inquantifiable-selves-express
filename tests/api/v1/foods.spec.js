28776c990896ae5c08803b947fbb34477e431795
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-06 13:45:43 -0600

    Adds GET api/v1/foods tests
    Co-authored-by: Blake Enyart 
    <38588295+blake-enyart@users.noreply.github.com>
    Co-authored-by: Vincent Provenzano 
    <40582072+Vjp888@users.noreply.github.com>

---
+++ tests/api/v1/foods.spec.js
@@ -0,0 +1,53 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../../../app');
+
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+  });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+  afterAll(() => {
+    shell.exec('npx sequelize db:drop')
+  });
+
+  describe('GET /api/v1/foods', () => {
+    test('It should return all food resources and 200 status code', async () => {
+      let response = await request(app).get("/api/v1/foods");
+      let foodsArray = response.body.foods;
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toHaveProperty("foods");
+      expect(foodsArray).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('GET /api/v1/foods/:id', () => {
+    test('It should return a single food resource and 200 status code', async () => {
+      let response = await request(app).get("/api/v1/foods/1");
+      let foodItem = response.body
+
+      expect(response.statusCode).toBe(200);
+      expect(foodItem).toHaveProperty("id");
+      expect(foodItem).toHaveProperty("name");
+      expect(foodItem).toHaveProperty("calories");
+    });
+  });
+
+  describe('GET /api/v1/foods/:id', () => {
+    test('It should return a 404 status code if :id is not present', async () => {
+      let response = await request(app).get("/api/v1/foods/1000");
+      let errorMessage = response.error.message
+
+      expect(response.statusCode).toBe(404);
+      expect(errorMessage).toBe("cannot GET /api/v1/foods/1000 (404)");
+    });
+  });
+
+});
